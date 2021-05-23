bb24c141ab6119727b682fd4de214c6b2ff5bcfa
MikuroXina <ryosukadnak@gmail.com>
2021-05-23 16:46:18 +0900

    feat: Impl derive and Debug

--- src/basis.rs
+++ src/basis.rs
@@ -1,16 +1,30 @@
 #![allow(dead_code)]
 
 /// `Color` は 24 ビットの RGB カラーを表す.
+#[derive(Clone)]
 pub(crate) struct Color {
     pub(crate) r: u8,
     pub(crate) g: u8,
     pub(crate) b: u8,
 }
 
+impl std::fmt::Debug for Color {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{:x}", self.r << 16 | self.g << 8 | self.b)
+    }
+}
+
 /// `Pos` は画像における座標を表す.
 ///
 /// フィールドの `u8` の上位 4 ビットに X 座標, 下位 4 ビットに Y 座標を格納する.
+#[derive(Clone, Copy)]
 pub(crate) struct Pos(u8);
+
+impl std::fmt::Debug for Pos {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "({}, {})", self.x(), self.y())
+    }
+}
 
 impl Pos {
     pub(crate) fn new(x: u8, y: u8) -> Self {
@@ -27,6 +41,7 @@ impl Pos {
 }
 
 /// `Movement` はある断片画像を動かして入れ替える向きを表す.
+#[derive(Debug, Clone, Copy)]
 pub(crate) enum Movement {
     Up,
     Right,
@@ -35,12 +50,14 @@ pub(crate) enum Movement {
 }
 
 /// `Operation` は座標 `select` の断片画像を選択してから `movements` の入れ替えを行う操作を表す.
+#[derive(Debug)]
 pub(crate) struct Operation {
     select: Pos,
     movements: Vec<Movement>,
 }
 
 /// `Dir` はある断片画像を原画像の状態から時計回りに回転させた角度を表す.
+#[derive(Debug, Clone, Copy)]
 pub(crate) enum Dir {
     R0,
     R90,
@@ -49,9 +66,11 @@ pub(crate) enum Dir {
 }
 
 /// `Edge` は画像上の連続する一直線のピクセル列を表す.
+#[derive(Debug, Clone)]
 pub(crate) struct Edge(pub(crate) Vec<Color>);
 
 /// `Fragment` は原画像から切り取った断片画像を表す. その座標 `pos` と縁四辺 `north_edge`, `east_edge`, `south_edge`, `west_edge` を表す.
+#[derive(Debug)]
 pub(crate) struct Fragment {
     pos: Pos,
     north_edges: Edge,
