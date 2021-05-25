3d4b777d09f7c87ba197ffb362cf5c2fde08f693
MikuroXina <ryosukadnak@gmail.com>
2021-05-25 16:38:11 +0900

    test: Improve test

--- src/fragment.rs
+++ src/fragment.rs
@@ -126,40 +126,41 @@ impl Fragment {
 mod tests {
     use {
         crate::{
-            basis::{Color, Problem, Rot},
+            basis::{Color, Rot},
             fragment::Fragment,
+            grid::Grid,
         },
         std::io::{self, Read, Result},
     };
 
+    fn pixels(to_skip: u64, width: usize, height: usize, path: &str) -> Result<Vec<Color>> {
+        let mut img = std::fs::File::open(path)?;
+        io::copy(&mut img.by_ref().take(to_skip), &mut io::sink())?;
+        let mut pixel_components = vec![0; width * height * 3];
+        img.read(&mut pixel_components)?;
+        Ok(pixel_components
+            .chunks(3)
+            .map(|comps| Color {
+                r: comps[0],
+                g: comps[1],
+                b: comps[2],
+            })
+            .collect())
+    }
+
     #[test]
     fn case1() -> Result<()> {
-        let mut img = std::fs::File::open("test_cases/02_sampled.ppm")?;
-        let to_skip = 32;
-        io::copy(&mut img.by_ref().take(to_skip), &mut io::sink())?;
-        let mut pixel_components = vec![0; 1_036_800];
-        img.read(&mut pixel_components)?;
-        let test_case = Problem {
-            select_limit: 5,
-            select_cost: 15,
-            swap_cost: 2,
-            width: 720,
-            height: 480,
-            rows: 3,
-            cols: 2,
-            pixels: pixel_components
-                .chunks(3)
-                .map(|comps| Color {
-                    r: comps[0],
-                    g: comps[1],
-                    b: comps[2],
-                })
-                .collect(),
-        };
+        let pixels = pixels(32, 180, 120, "test_cases/02_sampled.ppm")?;
+        let grid = Grid::new(3, 2);
+        let test_cases = &[
+            grid.clamping_pos(0, 0),
+            grid.clamping_pos(1, 1),
+            grid.clamping_pos(2, 1),
+        ];
 
-        let frags = Fragment::new_all(test_case);
+        for test_case in test_cases {
+            let frag = Fragment::new(&pixels, test_case.clone(), 180, 60, 60);
 
-        for frag in frags {
             assert!(matches!(frag.rot, Rot::R0));
         }
         Ok(())
