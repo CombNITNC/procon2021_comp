8963453dc431ec82864cdf7a5f8ce019ddb8ddfd
MikuroXina <ryosukadnak@gmail.com>
2021-05-25 17:17:25 +0900

    test: Split tests mod

--- src/fragment.rs
+++ src/fragment.rs
@@ -1,3 +1,6 @@
+#[cfg(test)]
+mod tests;
+
 use crate::{
     basis::{Color, Dir, Problem, Rot},
     grid::{Grid, Pos},
@@ -118,87 +121,5 @@ impl Fragment {
             rot: Rot::R0,
             edges: Edges::new(north, east, south, west),
         }
-    }
-}
-
-#[cfg(test)]
-mod tests {
-    use {
-        crate::{
-            basis::{Color, Dir, Rot},
-            fragment::Fragment,
-            grid::Grid,
-        },
-        std::io::{self, Read, Result},
-    };
-
-    fn pixels(to_skip: u64, width: usize, height: usize, path: &str) -> Result<Vec<Color>> {
-        let mut img = std::fs::File::open(path)?;
-        io::copy(&mut img.by_ref().take(to_skip), &mut io::sink())?;
-        let mut pixel_components = vec![0; width * height * 3];
-        img.read(&mut pixel_components)?;
-        Ok(pixel_components
-            .chunks(3)
-            .map(|comps| Color {
-                r: comps[0],
-                g: comps[1],
-                b: comps[2],
-            })
-            .collect())
-    }
-
-    #[test]
-    fn case1() -> Result<()> {
-        let width = 180;
-        let frag_edge = 60;
-        let rows = 3usize;
-        let cols = 2usize;
-        let pixels = pixels(32, width, 120, "test_cases/02_sampled.ppm")?;
-        let grid = Grid::new(rows as u8, cols as u8);
-
-        for y in 0..cols {
-            for x in 0..rows {
-                let pos = grid.clamping_pos(x as u8, y as u8);
-                let frag = Fragment::new(&pixels, pos.clone(), width, frag_edge as u16);
-
-                let up_left = x * frag_edge + y * frag_edge * width;
-
-                let edge = &frag.edges.0[0];
-                assert!(matches!(edge.dir, Dir::North));
-                for i in 0..60 {
-                    assert_eq!(&edge.pixels[i], &pixels[up_left + i]);
-                }
-
-                let edge = &frag.edges.0[1];
-                assert!(matches!(edge.dir, Dir::East));
-                for i in 0..60 {
-                    assert_eq!(
-                        &edge.pixels[i],
-                        &pixels[up_left + (frag_edge - 1) + i * width]
-                    );
-                }
-
-                let edge = &frag.edges.0[2];
-                assert!(matches!(edge.dir, Dir::South));
-                for i in 0..60 {
-                    assert_eq!(
-                        &edge.pixels[i],
-                        &pixels[up_left + (frag_edge - 1) * width + (frag_edge - 1) - i]
-                    );
-                }
-
-                let edge = &frag.edges.0[3];
-                assert!(matches!(edge.dir, Dir::West));
-                for i in 0..60 {
-                    assert_eq!(
-                        &edge.pixels[i],
-                        &pixels[up_left + (frag_edge - 1) * width - i * width]
-                    );
-                }
-
-                assert!(matches!(frag.rot, Rot::R0));
-            }
-        }
-        Ok(())
     }
 }
