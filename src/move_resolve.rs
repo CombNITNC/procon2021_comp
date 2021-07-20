48d315f81a2bb032d6ed138a3e07d56d03e5ce00
MikuroXina <ryosukadnak@gmail.com>
2021-07-20 22:39:40 +0900

    feat: Impl State for GridState

--- src/move_resolve.rs
+++ src/move_resolve.rs
@@ -1,4 +1,4 @@
-use self::edges_nodes::EdgesNodes;
+use self::{edges_nodes::EdgesNodes, ida_star::State};
 use crate::{
     basis::Operation,
     grid::{Grid, Pos, VecOnGrid},
@@ -14,13 +14,15 @@ mod ida_star;
 mod tests;
 
 #[derive(Debug, Clone)]
-struct State {
+struct GridState {
     grid: Grid,
     field: VecOnGrid<'static, Pos>,
     selecting: Pos,
+    swap_cost: u64,
+    select_cost: u64,
 }
 
-fn h1(state: &State) -> u64 {
+fn h1(state: &GridState) -> u64 {
     let mut edges = vec![];
     for pos in state.grid.all_pos() {
         if let Some(right) = state.grid.right_of(pos) {
@@ -51,16 +53,42 @@ fn h1(state: &State) -> u64 {
         .count() as u64
 }
 
-fn heuristic(state: &State) -> u64 {
-    // h1 = 隣接マスどうしのマンハッタン距離が 1 かつ全頂点がゴール位置に無い集合の数
-    // h2 = ゴール位置と異なる位置のマスの数
-    let h1: u64 = h1(state);
-    let h2: u64 = state
-        .grid
-        .all_pos()
-        .filter(|&pos| pos != state.field[pos])
-        .count() as u64;
-    h1 + h2
+impl State<Vec<GridState>, u64> for GridState {
+    fn next_states(&self) -> Vec<GridState> {
+        self.grid
+            .all_pos()
+            .map(|next_select| Self {
+                selecting: next_select,
+                ..self.clone()
+            })
+            .collect()
+    }
+
+    fn is_goal(&self) -> bool {
+        self.grid
+            .all_pos()
+            .map(|pos| (pos, self.field[pos]))
+            .all(|(pos, cell)| pos == cell)
+    }
+
+    fn heuristic(&self) -> u64 {
+        // h1 = 隣接マスどうしのマンハッタン距離が 1 かつ全頂点がゴール位置に無い集合の数
+        // h2 = ゴール位置と異なる位置のマスの数
+        let h1: u64 = h1(self);
+        let h2: u64 = self
+            .grid
+            .all_pos()
+            .filter(|&pos| pos != self.field[pos])
+            .count() as u64;
+        h1 + h2
+    }
+
+    fn cost_between(&self, next: &Self) -> u64 {
+        if self.selecting.manhattan_distance(next.selecting) == 1 {
+            return self.swap_cost;
+        }
+        self.swap_cost + self.select_cost
+    }
 }
 
 /// 完成形から `movements` のとおりに移動されているとき, それを解消する移動手順を求める.
