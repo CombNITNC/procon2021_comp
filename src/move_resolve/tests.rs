c0d81eb9103cc96164f969ada0d66ad8f433dcad
MikuroXina <ryosukadnak@gmail.com>
2021-08-07 18:40:53 +0900

    fix: Make simple_case simple

--- src/move_resolve/tests.rs
+++ src/move_resolve/tests.rs
@@ -26,45 +26,15 @@ fn test_h1() {
 
 #[test]
 fn simple_case() {
-    //  00   11
-    // [10]  01
+    // 00 11
+    // 10 01
     let grid = Grid::new(2, 2);
     let mut field = VecOnGrid::with_init(&grid, grid.pos(0, 0));
     field[grid.pos(0, 0)] = grid.pos(0, 0);
     field[grid.pos(1, 0)] = grid.pos(1, 1);
     field[grid.pos(0, 1)] = grid.pos(1, 0);
     field[grid.pos(1, 1)] = grid.pos(0, 1);
-    let state = GridState {
-        grid: &grid,
-        field,
-        selecting: Some(grid.pos(0, 1)),
-        swap_cost: 1,
-        select_cost: 1,
-    };
-    let next_states = state.next_states();
-    assert_eq!(next_states.len(), 5);
-    // [10]  11
-    //  00   01
-    assert_eq!(next_states[0].field[grid.pos(0, 0)], grid.pos(1, 0));
-    assert_eq!(next_states[0].field[grid.pos(0, 1)], grid.pos(0, 0));
-    assert_eq!(next_states[0].selecting.unwrap(), grid.pos(0, 0));
-    // 00  11
-    // 01 [10]
-    assert_eq!(next_states[1].field[grid.pos(0, 1)], grid.pos(0, 1));
-    assert_eq!(next_states[1].field[grid.pos(1, 1)], grid.pos(1, 0));
-    assert_eq!(next_states[1].selecting.unwrap(), grid.pos(1, 1));
-    // [00]  11
-    //  10   01
-    assert_eq!(next_states[2].selecting.unwrap(), grid.pos(0, 0));
-    //  00  [11]
-    //  10   01
-    assert_eq!(next_states[3].selecting.unwrap(), grid.pos(1, 0));
-    //  00   11
-    //  10  [01]
-    assert_eq!(next_states[4].selecting.unwrap(), grid.pos(1, 1));
 
-    // 00 11
-    // 10 01
     let path = resolve(
         &grid,
         &[
