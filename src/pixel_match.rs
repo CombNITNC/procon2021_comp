60ec6be64e18c429714e74ca8aa816a6074cd794
kawaemon <me@kawaemon.dev>
2021-09-04 05:06:16 +0900

    feat: resolver

--- src/pixel_match.rs
+++ src/pixel_match.rs
@@ -1,9 +1,103 @@
-use crate::basis::{Color, Dir, Rot};
+use crate::basis::{Color, Dir, Problem, Rot};
 use crate::fragment::Fragment;
-use crate::grid::{Pos, VecOnGrid};
+use crate::grid::{Grid, Pos, VecOnGrid};
 
 mod double_side;
 mod shaker;
+
+use double_side::fill_by_double_side;
+use shaker::shaker_fill;
+
+pub(crate) fn resolve<'g>(problem: &Problem, grid: &'g Grid) -> VecOnGrid<'g, Option<Fragment>> {
+    let mut fragments = Fragment::new_all(problem);
+    let mut fragment_grid = VecOnGrid::<Option<Fragment>>::with_default(grid);
+
+    // 必ず向きの正しい左上の断片を取得
+    let root = find_and_remove(&mut fragments, Pos::new(0, 0)).unwrap();
+
+    // そこから上下左右に伸ばす形で探索
+    let (up, down) = shaker_fill(
+        grid.height(),
+        &mut fragments,
+        (Dir::North, Dir::South),
+        (&root, &root),
+    );
+    let (left, right) = shaker_fill(
+        grid.width(),
+        &mut fragments,
+        (Dir::West, Dir::East),
+        (&root, &root),
+    );
+
+    // root から上下左右に何個断片が有るかわかったので、rootのあるべき座標が分かる
+    let root_pos = Pos::new(left.len() as _, up.len() as _);
+
+    place_initial_result_on_grid(&mut fragment_grid, root, [up, down, left, right]);
+
+    // r = root, x = すでにわかった断片 としたとき、今以下のような状態になっている。
+    // ------------
+    //    2    x  1
+    //         x
+    // xxxxxxxxrxxx
+    //    3    x  4
+    // ------------
+    // この 1,2,3,4 で示したスペースをそれぞれ root に近い断片から埋めていく。
+    // 2辺わかった状態で探索できるため、精度向上が期待できる。
+
+    // 1
+    for x in root_pos.x() + 1..grid.width() {
+        for y in (0..root_pos.y()).rev() {
+            fill_by_double_side(
+                &mut fragments,
+                &mut fragment_grid,
+                Pos::new(x, y),
+                (Pos::new(x, y + 1), Dir::North),
+                (Pos::new(x - 1, y), Dir::East),
+            );
+        }
+    }
+
+    // 2
+    for x in (0..root_pos.x()).rev() {
+        for y in (0..root_pos.y()).rev() {
+            fill_by_double_side(
+                &mut fragments,
+                &mut fragment_grid,
+                Pos::new(x, y),
+                (Pos::new(x + 1, y), Dir::West),
+                (Pos::new(x, y + 1), Dir::North),
+            );
+        }
+    }
+
+    // 3
+    for x in (0..root_pos.x()).rev() {
+        for y in root_pos.y() + 1..grid.height() {
+            fill_by_double_side(
+                &mut fragments,
+                &mut fragment_grid,
+                Pos::new(x, y),
+                (Pos::new(x, y - 1), Dir::South),
+                (Pos::new(x + 1, y), Dir::West),
+            );
+        }
+    }
+
+    // 4
+    for x in root_pos.x() + 1..grid.width() {
+        for y in root_pos.y() + 1..grid.height() {
+            fill_by_double_side(
+                &mut fragments,
+                &mut fragment_grid,
+                Pos::new(x, y),
+                (Pos::new(x - 1, y), Dir::East),
+                (Pos::new(x, y - 1), Dir::South),
+            );
+        }
+    }
+
+    fragment_grid
+}
 
 #[derive(Debug)]
 struct DiffEntry {
