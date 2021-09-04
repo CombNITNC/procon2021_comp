b1273c08577eded8d506a54a47b4865332e50a9e
MikuroXina <ryosukadnak@gmail.com>
2021-09-05 06:06:34 +0900

    feat: Add move resolver templates

--- src/move_resolve.rs
+++ src/move_resolve.rs
@@ -240,126 +240,35 @@ pub(crate) fn resolve(
     actions_to_operations(path)
 }
 
-#[derive(Clone)]
-struct RowCompleter<'grid> {
-    field: VecOnGrid<'grid, Pos>,
-    selecting: Option<Pos>,
-    prev_action: Option<GridAction>,
-    target_row: u8,
-    swap_cost: u16,
-    select_cost: u16,
-    remaining_select: u8,
+fn least_movements(x: i32, y: i32) -> u32 {
+    if x == 0 && y == 0 {
+        return 0;
+    }
+    let x = x.abs();
+    let y = y.abs();
+    let d = (x - y).unsigned_abs();
+    let m = x.min(y) as u32;
+    let mut ret = 5 * d + 6 * m - 4;
+    if x == y {
+        ret += 2;
+    }
+    ret
 }
 
-impl std::fmt::Debug for RowCompleter<'_> {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        f.debug_struct("GridState")
-            .field("field", &self.field)
-            .field("selecting", &self.selecting)
-            .field("target_row", &self.target_row)
-            .field("remaining_select", &self.remaining_select)
-            .finish()
-    }
+fn path_to_move_select_around_target(
+    field: &VecOnGrid<Pos>,
+    target: Pos,
+    select: Pos,
+) -> Vec<GridAction> {
+    // ダイクストラ法で select を target の隣へ動かす経路を決定する.
+    // コストは各マスの必要最低手数の合計.
+    todo!()
 }
 
-impl IdaStarState for RowCompleter<'_> {
-    type A = GridAction;
-    fn apply(&self, action: Self::A) -> Self {
-        match action {
-            GridAction::Swap(mov) => {
-                let selecting = self.selecting.unwrap();
-                let next_swap = self.field.grid.move_pos_to(selecting, mov);
-                let mut new_field = self.field.clone();
-                new_field.swap(selecting, next_swap);
-                Self {
-                    selecting: Some(next_swap),
-                    field: new_field,
-                    prev_action: Some(action),
-                    ..self.clone()
-                }
-            }
-            GridAction::Select(sel) => Self {
-                selecting: Some(sel),
-                remaining_select: self.remaining_select - 1,
-                prev_action: Some(action),
-                ..self.clone()
-            },
-        }
-    }
-
-    type AS = Vec<Self::A>;
-    fn next_actions(&self) -> Self::AS {
-        // Y 座標が target_row であるマスを揃える
-        let grid = self.field.grid;
-        let different = grid.all_pos().filter(|&pos| pos != self.field[pos]);
-        if self.prev_action.is_none() {
-            return different.map(GridAction::Select).collect();
-        }
-        let selecting = self.selecting.unwrap();
-        let prev = self.prev_action.unwrap();
-        let swapping_states = [
-            Movement::Up,
-            Movement::Right,
-            Movement::Down,
-            Movement::Left,
-        ]
-        .iter()
-        .copied()
-        .filter(|&around| {
-            if let GridAction::Swap(dir) = prev {
-                around != dir.opposite()
-            } else {
-                true
-            }
-        })
-        .filter(|&mov| {
-            let next_to_swap = grid.move_pos_to(selecting, mov);
-            if self.field[next_to_swap].y() != self.target_row {
-                return true;
-            }
-            // 入れ替え対象が target_row に属する場合
-            // next_to_swap がマンハッタン経路を辿るようにする
-            let min_vec = grid.looping_min_vec(next_to_swap, self.field[next_to_swap]);
-            let preferred_dir = match min_vec {
-                (min_x, min_y) if 0 < min_x && 0 < min_y => [Movement::Right, Movement::Down],
-                (min_x, min_y) if 0 < min_x && min_y < 0 => [Movement::Right, Movement::Up],
-                (min_x, min_y) if min_x < 0 && 0 < min_y => [Movement::Left, Movement::Down],
-                _ => [Movement::Left, Movement::Up],
-            };
-            let dir = Movement::between_pos(next_to_swap, selecting);
-            preferred_dir.iter().any(|&preferred| preferred == dir)
-        })
-        .map(GridAction::Swap);
-        if matches!(prev, GridAction::Swap(_)) && 2 < self.remaining_select {
-            let selecting_states = different
-                .filter(|&p| p != selecting)
-                .map(GridAction::Select);
-            swapping_states.chain(selecting_states).collect()
-        } else {
-            swapping_states.collect()
-        }
-    }
-    fn is_goal(&self) -> bool {
-        let grid = self.field.grid;
-        (0..grid.width())
-            .map(|x| grid.clamping_pos(x, self.target_row))
-            .all(|pos| pos == self.field[pos])
-    }
-
-    type C = u64;
-    fn heuristic(&self) -> Self::C {
-        let grid = self.field.grid;
-        (0..grid.width())
-            .map(|x| grid.clamping_pos(x, self.target_row))
-            .filter(|&pos| pos != self.field[pos])
-            .count() as u64
-    }
-    fn cost_on(&self, action: Self::A) -> Self::C {
-        match action {
-            GridAction::Swap(_) => self.swap_cost as u64,
-            GridAction::Select(_) => self.select_cost as u64,
-        }
-    }
+fn path_to_move_target_to_goal(field: &VecOnGrid<Pos>, target: Pos) -> Vec<GridAction> {
+    // ダイクストラ法で target をゴール位置へ動かす経路を決定する.
+    // コストは各マスの必要最低手数の合計.
+    todo!()
 }
 
 /// 完成形から `movements` のとおりに移動されているとき, それを解消する移動手順の近似解を求める.
@@ -384,32 +293,16 @@ fn resolve_approximately(
         };
         row_to_sort.sort_by(|&a, &b| cost_to_sort_row(a).cmp(&cost_to_sort_row(b)));
         let y = row_to_sort.pop().unwrap();
-        eprintln!("start to sort the row: {}", y);
-        let row_completer = RowCompleter {
-            field: nodes.clone(),
-            selecting: selection,
-            target_row: y,
-            prev_action: all_actions.last().copied(),
-            swap_cost,
-            select_cost,
-            remaining_select: select_limit,
-        };
-        let (mut actions, _cost) = ida_star(row_completer, 0);
-        for &action in &actions {
-            match action {
-                GridAction::Swap(mov) => {
-                    let sel = selection.unwrap();
-                    let swap_to = grid.move_pos_to(sel, mov);
-                    nodes.swap(sel, swap_to);
-                }
-                GridAction::Select(sel) => {
-                    selection = Some(sel);
-                    select_limit -= 1;
-                }
+
+        for x in 0..grid.width() {
+            let target = grid.clamping_pos(x, y);
+            if target != nodes[target] {
+                let mut actions = path_to_move_target_to_goal(&nodes, target);
+                all_actions.append(&mut actions);
             }
         }
-        eprintln!("sort result: {:?}", actions);
-        all_actions.append(&mut actions);
+
+        eprintln!("sort result: {:?}", nodes);
     }
     let different_cells = DifferentCells::new(&nodes);
     let (mut actions, _total_cost) = ida_star(
