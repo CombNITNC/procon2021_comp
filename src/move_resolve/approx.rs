89b1b327c1b855fc0ae1fa904f2ab3bdf5672412
MikuroXina <ryosukadnak@gmail.com>
2021-09-06 01:29:23 +0900

    feat: Impl solve_row

--- src/move_resolve/approx.rs
+++ src/move_resolve/approx.rs
@@ -1,15 +1,59 @@
 use std::ops;
 
+use self::estimate::{estimate_solve_row, RowSolveEstimate};
+use super::GridAction;
 use crate::{
     basis::Movement,
-    grid::{Pos, VecOnGrid},
+    grid::{board::Board, Pos, VecOnGrid},
 };
 
 mod estimate;
 mod route;
 
-pub(crate) fn solve_row(field: &VecOnGrid<Pos>, target_row: u8) -> Vec<Movement> {
-    todo!()
+#[derive(Debug, Default)]
+pub(crate) struct Solver {
+    estimate: Option<RowSolveEstimate>,
+}
+
+impl Solver {
+    pub(super) fn solve_row(
+        &mut self,
+        select: Pos,
+        field: &VecOnGrid<Pos>,
+        target_row: u8,
+    ) -> Vec<GridAction> {
+        let targets: Vec<_> = (0..field.grid.width())
+            .map(move |x| field.grid.clamping_pos(x, target_row))
+            .filter(|&p| p != field[p])
+            .collect();
+
+        let estimate = estimate_solve_row(Board::new(select, field.clone()), &targets);
+        if let Some(worst_estimate) = &self.estimate {
+            if worst_estimate.worst_route_size < estimate.worst_route_size {
+                self.estimate.replace(estimate);
+            }
+        } else {
+            self.estimate.replace(estimate);
+        }
+        let estimate = self.estimate.as_ref().unwrap();
+
+        let mut actions = vec![];
+        if field
+            .grid
+            .looping_manhattan_dist(estimate.moves[0], estimate.moves[1])
+            != 1
+        {
+            actions.push(GridAction::Select(estimate.moves[0]));
+        }
+        for win in estimate.moves.windows(2) {
+            if field.grid.looping_manhattan_dist(win[0], win[1]) == 1 {
+                actions.push(GridAction::Swap(Movement::between_pos(win[0], win[1])));
+            } else {
+                actions.push(GridAction::Select(win[1]));
+            }
+        }
+        actions
+    }
 }
 
 fn least_movements((dx, dy): (i32, i32)) -> u32 {
