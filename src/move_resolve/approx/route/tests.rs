7c035d85f49aa4130d0bdd7a5566efa6a28116e6
MikuroXina <ryosukadnak@gmail.com>
2021-09-06 14:12:51 +0900

    test: Fix test case

--- src/move_resolve/approx/route/tests.rs
+++ src/move_resolve/approx/route/tests.rs
@@ -1,5 +1,5 @@
 use crate::{
-    grid::{board::Board, Grid},
+    grid::{board::Board, Grid, RangePos},
     move_resolve::edges_nodes::EdgesNodes,
 };
 
@@ -10,19 +10,25 @@ fn test_route_target_to_goal() {
     // 22 10 20 30 40
     // 01 11 21[32]41
     // 02 12[00]31 42
-    // 02 12 22 32 42
-    // 02 12 22 32 42
-    let grid = Grid::new(4, 3);
-    let movements = &[(grid.pos(0, 0), grid.pos(2, 2))];
+    // 03 13 23 33 43
+    // 04 14 24 34 44
+    let grid = Grid::new(5, 5);
+    let movements = &[
+        (grid.pos(0, 0), grid.pos(2, 2)),
+        (grid.pos(2, 2), grid.pos(0, 0)),
+        (grid.pos(3, 1), grid.pos(3, 2)),
+        (grid.pos(3, 2), grid.pos(3, 1)),
+    ];
     let EdgesNodes { nodes, .. } = EdgesNodes::new(&grid, movements);
-    let board = Board::new(grid.pos(1, 1), nodes);
+    let board = Board::new(grid.pos(3, 1), nodes);
 
-    let actual = route_target_to_goal(&board, grid.pos(0, 1), grid.all_pos()).unwrap();
+    let actual =
+        route_target_to_goal(&board, grid.pos(2, 2), RangePos::single(grid.pos(0, 0))).unwrap();
 
     let expected = vec![
         grid.pos(2, 2),
-        grid.pos(1, 2),
-        grid.pos(0, 2),
+        grid.pos(2, 1),
+        grid.pos(1, 1),
         grid.pos(0, 1),
         grid.pos(0, 0),
     ];
@@ -32,6 +38,6 @@ fn test_route_target_to_goal() {
         .zip(actual.iter())
         .enumerate()
         .for_each(|(i, (e, a))| {
-            assert_eq!(e, a, "{:?} {:?}, index: {}", e, a, i);
+            assert_eq!(e, a, "index: {}", i);
         });
 }
