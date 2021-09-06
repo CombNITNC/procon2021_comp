b5e6970ae236a1bcb2c1b491652391fb6e910849
MikuroXina <ryosukadnak@gmail.com>
2021-09-06 15:31:00 +0900

    test: Add test_route_target_to_pos

--- src/move_resolve/approx/route/tests.rs
+++ src/move_resolve/approx/route/tests.rs
@@ -3,7 +3,43 @@ use crate::{
     move_resolve::edges_nodes::EdgesNodes,
 };
 
-use super::route_target_to_goal;
+use super::{route_target_to_goal, route_target_to_pos};
+
+#[test]
+fn test_route_target_to_pos() {
+    // 22 10 20 30 40
+    // 01 11 21[32]41
+    // 02 12[00]31 42
+    // 03 13 23 33 43
+    // 04 14 24 34 44
+    let grid = Grid::new(5, 5);
+    let movements = &[
+        (grid.pos(0, 0), grid.pos(2, 2)),
+        (grid.pos(2, 2), grid.pos(0, 0)),
+        (grid.pos(3, 1), grid.pos(3, 2)),
+        (grid.pos(3, 2), grid.pos(3, 1)),
+    ];
+    let EdgesNodes { nodes, .. } = EdgesNodes::new(&grid, movements);
+    let board = Board::new(grid.pos(3, 1), nodes);
+
+    let actual = route_target_to_pos(&board, grid.pos(2, 2), grid.pos(0, 0)).unwrap();
+
+    let expected = vec![
+        grid.pos(2, 2),
+        grid.pos(2, 1),
+        grid.pos(1, 1),
+        grid.pos(0, 1),
+        grid.pos(0, 0),
+    ];
+    assert_eq!(expected.len(), actual.len(), "{:?} {:?}", expected, actual);
+    expected
+        .iter()
+        .zip(actual.iter())
+        .enumerate()
+        .for_each(|(i, (e, a))| {
+            assert_eq!(e, a, "index: {}", i);
+        });
+}
 
 #[test]
 fn test_route_target_to_goal() {
