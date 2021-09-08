cf5082257a10a0b895c12c314bd657af48df271e
MikuroXina <ryosukadnak@gmail.com>
2021-09-08 12:21:15 +0900

    fix: Fix estimate arguments

--- src/move_resolve/approx/estimate.rs
+++ src/move_resolve/approx/estimate.rs
@@ -17,7 +17,7 @@ pub(super) fn estimate_solve_row(mut board: Board, target_row: u8) -> RowSolveEs
         .collect();
     let mut estimate = RowSolveEstimate::default();
 
-    let line_proc = estimate_line_without_edge(board.clone(), &targets);
+    let line_proc = estimate_line_without_edge(board.clone(), &targets[..targets.len() - 2]);
     for mov in line_proc.moves {
         estimate.moves.push(mov);
         board.swap_to(mov);
@@ -30,8 +30,14 @@ pub(super) fn estimate_solve_row(mut board: Board, target_row: u8) -> RowSolveEs
         estimate.worst_swap_pos = line_proc.worst_swap_pos;
     }
 
-    let edge_rd_estimate = estimate_edge_then_right_down(&board, &targets);
-    let edge_ld_estimate = estimate_edge_then_left_down(&board, &targets);
+    let edge_rd_estimate = estimate_edge_then_right_down(
+        &board,
+        (targets[targets.len() - 2], targets[targets.len() - 1]),
+    );
+    let edge_ld_estimate = estimate_edge_then_left_down(
+        &board,
+        (targets[targets.len() - 2], targets[targets.len() - 1]),
+    );
     let mut edge_estimate = if edge_rd_estimate.len() < edge_ld_estimate.len() {
         edge_rd_estimate
     } else {
@@ -74,15 +80,13 @@ fn estimate_line_without_edge(mut board: Board, targets: &[Pos]) -> RowSolveEsti
 /// ... ** b
 /// ```
 /// この形に変形してから `Right` → `Down` して行を完成させる経路を見積もる
-fn estimate_edge_then_right_down(board: &Board, targets: &[Pos]) -> Vec<Pos> {
+fn estimate_edge_then_right_down(board: &Board, (a, b): (Pos, Pos)) -> Vec<Pos> {
     let mut board = board.clone();
     let mut ret = vec![];
 
-    let a = targets[targets.len() - 1];
     let a_goal = board.grid().right_of(a);
     move_target_to_pos(&mut board, a, a_goal, &mut ret);
 
-    let b = targets.last().copied().unwrap();
     let b_goal = board.grid().down_of(b);
     move_target_to_pos(&mut board, b, b_goal, &mut ret);
 
@@ -100,15 +104,13 @@ fn estimate_edge_then_right_down(board: &Board, targets: &[Pos]) -> Vec<Pos> {
 /// ... a **
 /// ```
 /// この形に変形してから `Left` → `Down` して行を完成させる経路を見積もる
-fn estimate_edge_then_left_down(board: &Board, targets: &[Pos]) -> Vec<Pos> {
+fn estimate_edge_then_left_down(board: &Board, (a, b): (Pos, Pos)) -> Vec<Pos> {
     let mut board = board.clone();
     let mut ret = vec![];
 
-    let a = targets[targets.len() - 1];
     let a_goal = board.grid().down_of(a);
     move_target_to_pos(&mut board, a, a_goal, &mut ret);
 
-    let b = targets.last().copied().unwrap();
     let b_goal = board.grid().left_of(b);
     move_target_to_pos(&mut board, b, b_goal, &mut ret);
 
