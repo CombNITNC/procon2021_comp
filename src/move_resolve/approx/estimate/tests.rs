9f6dcfe67e9a372b71ff8e4aa25bad5b49eb81e0
MikuroXina <ryosukadnak@gmail.com>
2021-09-10 03:22:52 +0900

    test: Fix test case and Add tests

--- src/move_resolve/approx/estimate/tests.rs
+++ src/move_resolve/approx/estimate/tests.rs
@@ -19,15 +19,35 @@ fn test_estimate_solve_row() {
         (grid.pos(3, 2), grid.pos(3, 1)),
     ];
     let EdgesNodes { nodes, .. } = EdgesNodes::new(&grid, movements);
-    let board = Board::new(grid.pos(3, 2), nodes);
-    let actual = estimate_solve_row(board, 0);
+    let mut board = Board::new(grid.pos(3, 2), nodes);
+    let actual = estimate_solve_row(board.clone(), 0);
 
     let expected = vec![
-        grid.pos(2, 2),
+        grid.pos(3, 2),
+        grid.pos(3, 1),
         grid.pos(2, 1),
+        grid.pos(2, 2),
+        grid.pos(1, 2),
+        grid.pos(1, 1),
+        grid.pos(2, 1),
+        grid.pos(3, 1),
+        grid.pos(4, 1),
+        grid.pos(0, 1),
         grid.pos(1, 1),
         grid.pos(1, 0),
         grid.pos(0, 0),
+        grid.pos(0, 1),
+        grid.pos(4, 1),
+        grid.pos(4, 0),
+        grid.pos(3, 0),
+        grid.pos(2, 0),
+        grid.pos(1, 0),
+        grid.pos(1, 1),
+        grid.pos(2, 1),
+        grid.pos(2, 0),
+        grid.pos(3, 0),
+        grid.pos(4, 0),
+        grid.pos(4, 1),
     ];
     assert_eq!(
         expected.len(),
@@ -41,4 +61,10 @@ fn test_estimate_solve_row() {
         .zip(actual.moves.iter())
         .enumerate()
         .for_each(|(i, (e, a))| assert_eq!(e, a, "index: {}", i));
+
+    board.swap_many_to(&actual.moves);
+    for x in 0..grid.width() {
+        let pos = grid.pos(x, 0);
+        assert_eq!(pos, board.forward(pos));
+    }
 }
