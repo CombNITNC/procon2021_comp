9495480cb6b645748eb81de58df3a4899c73304e
MikuroXina <ryosukadnak@gmail.com>
2021-09-12 15:58:03 +0900

    test: Add large_case2

--- src/move_resolve/tests.rs
+++ src/move_resolve/tests.rs
@@ -215,6 +215,72 @@ fn large_case1() {
 }
 
 #[test]
+fn large_case2() {
+    // 10 20 30 40 50 00
+    // 11 21 31 41 51 01
+    // 12 22 32 42 52 02
+    // 13 23 33 43 53 03
+    // 14 24 34 44 54 04
+    // 15 25 35 45 55 05
+    let grid = Grid::new(6, 6);
+    let case = &[
+        (grid.pos(1, 0), grid.pos(0, 0)),
+        (grid.pos(1, 1), grid.pos(0, 1)),
+        (grid.pos(1, 2), grid.pos(0, 2)),
+        (grid.pos(1, 3), grid.pos(0, 3)),
+        (grid.pos(1, 4), grid.pos(0, 4)),
+        (grid.pos(1, 5), grid.pos(0, 5)),
+        (grid.pos(2, 0), grid.pos(1, 0)),
+        (grid.pos(2, 1), grid.pos(1, 1)),
+        (grid.pos(2, 2), grid.pos(1, 2)),
+        (grid.pos(2, 3), grid.pos(1, 3)),
+        (grid.pos(2, 4), grid.pos(1, 4)),
+        (grid.pos(2, 5), grid.pos(1, 5)),
+        (grid.pos(3, 0), grid.pos(2, 0)),
+        (grid.pos(3, 1), grid.pos(2, 1)),
+        (grid.pos(3, 2), grid.pos(2, 2)),
+        (grid.pos(3, 3), grid.pos(2, 3)),
+        (grid.pos(3, 4), grid.pos(2, 4)),
+        (grid.pos(3, 5), grid.pos(2, 5)),
+        (grid.pos(4, 0), grid.pos(3, 0)),
+        (grid.pos(4, 1), grid.pos(3, 1)),
+        (grid.pos(4, 2), grid.pos(3, 2)),
+        (grid.pos(4, 3), grid.pos(3, 3)),
+        (grid.pos(4, 4), grid.pos(3, 4)),
+        (grid.pos(4, 5), grid.pos(3, 5)),
+        (grid.pos(5, 0), grid.pos(4, 0)),
+        (grid.pos(5, 1), grid.pos(4, 1)),
+        (grid.pos(5, 2), grid.pos(4, 2)),
+        (grid.pos(5, 3), grid.pos(4, 3)),
+        (grid.pos(5, 4), grid.pos(4, 4)),
+        (grid.pos(5, 5), grid.pos(4, 5)),
+        (grid.pos(0, 0), grid.pos(5, 0)),
+        (grid.pos(0, 1), grid.pos(5, 1)),
+        (grid.pos(0, 2), grid.pos(5, 2)),
+        (grid.pos(0, 3), grid.pos(5, 3)),
+        (grid.pos(0, 4), grid.pos(5, 4)),
+        (grid.pos(0, 5), grid.pos(5, 5)),
+    ];
+    let Nodes { mut nodes, .. } = Nodes::new(grid, case);
+    const SELECT_LIMIT: u8 = 3;
+    const SWAP_COST: u16 = 1;
+    const SELECT_COST: u16 = 8;
+
+    let result = resolve(grid, case, SELECT_LIMIT, SWAP_COST, SELECT_COST);
+
+    let finder = BoardFinder::new(grid);
+    for Operation { select, movements } in result {
+        let mut current = select;
+        for movement in movements {
+            let to_swap = finder.move_pos_to(current, movement);
+            nodes.swap(current, to_swap);
+            current = to_swap;
+        }
+    }
+    assert!(grid.all_pos().zip(nodes.into_iter()).all(|(p, n)| p == n));
+}
+
+#[test]
 fn rand_case() {
     fn gen_circular(grid: Grid, rng: &mut rand::rngs::ThreadRng) -> Vec<Pos> {
         use rand::{
