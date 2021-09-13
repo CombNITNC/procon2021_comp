d605a9d696db9d850682a3c81b91a209dcf61114
MikuroXina <ryosukadnak@gmail.com>
2021-09-13 19:48:37 +0900

    test: Add case1

--- src/kaitou.rs
+++ src/kaitou.rs
@@ -11,7 +11,7 @@ pub(crate) fn ans(ope: &[Operation], rot: &[Rot]) -> String {
             Rot::R180 => 2,
             Rot::R270 => 3,
         };
-        result += &format!("{}",r);
+        result += &format!("{}", r);
     }
     result += "/r/n";
 
@@ -39,4 +39,48 @@ pub(crate) fn ans(ope: &[Operation], rot: &[Rot]) -> String {
     }
 
     result
+}
+
+#[test]
+fn case1() {
+    use crate::grid::Grid;
+
+    let grid = Grid::new(12, 2);
+
+    let expected = "01230320111103230210\r\n1\r\nA0\r\n3\r\nUDLR".to_owned();
+    let actual = ans(
+        &[Operation {
+            select: grid.pos(10, 1),
+            movements: vec![
+                Movement::Up,
+                Movement::Down,
+                Movement::Left,
+                Movement::Right,
+            ],
+        }],
+        &[
+            Rot::R0,
+            Rot::R90,
+            Rot::R180,
+            Rot::R270,
+            Rot::R0,
+            Rot::R270,
+            Rot::R180,
+            Rot::R0,
+            Rot::R90,
+            Rot::R90,
+            Rot::R90,
+            Rot::R90,
+            Rot::R0,
+            Rot::R270,
+            Rot::R180,
+            Rot::R270,
+            Rot::R0,
+            Rot::R180,
+            Rot::R90,
+            Rot::R0,
+        ],
+    );
+
+    assert_eq!(expected, actual);
 }
