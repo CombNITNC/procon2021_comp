6d2b776aa059d93d730d72c70f7be1f5fe83360a
Mikuroさいな <ryosukadnak@gmail.com>
2021-09-13 19:55:56 +0900

    kaitou のバグ修正とテストの追加 (#31)
    * test: Add case1
    
    * fix: Fix ans
    
    * test: Fix test case

--- src/kaitou.rs
+++ src/kaitou.rs
@@ -1,5 +1,7 @@
 use crate::basis::{Movement, Operation, Rot};
 
+const NEW_LINE: &str = "\r\n";
+
 pub(crate) fn ans(ope: &[Operation], rot: &[Rot]) -> String {
     let mut result = String::new();
 
@@ -11,19 +13,22 @@ pub(crate) fn ans(ope: &[Operation], rot: &[Rot]) -> String {
             Rot::R180 => 2,
             Rot::R270 => 3,
         };
-        result += &format!("{}",r);
+        result += &r.to_string();
     }
-    result += "/r/n";
+    result += NEW_LINE;
 
     //選択回数
-    result += &format!("{}/r/n", ope.len());
+    result += &ope.len().to_string();
+    result += NEW_LINE;
 
     for i in ope {
         //選択画像位置
-        result += &format!("{}{}/r/n", i.select.x(), i.select.y());
+        result += &format!("{:X}{:X}", i.select.x(), i.select.y());
+        result += NEW_LINE;
 
         //交換回数
-        result += &format!("{}/r/n", i.movements.len());
+        result += &i.movements.len().to_string();
+        result += NEW_LINE;
 
         //交換操作
         for j in &i.movements {
@@ -35,8 +40,52 @@ pub(crate) fn ans(ope: &[Operation], rot: &[Rot]) -> String {
             };
             result.push(m);
         }
-        result += "/r/n";
+        result += NEW_LINE;
     }
 
     result
+}
+
+#[test]
+fn case1() {
+    use crate::grid::Grid;
+
+    let grid = Grid::new(12, 2);
+
+    let expected = "01230320111103230210\r\n1\r\nA1\r\n4\r\nUDLR\r\n".to_owned();
+    let actual = ans(
+        &[Operation {
+            select: grid.pos(10, 1),
+            movements: vec![
+                Movement::Up,
+                Movement::Down,
+                Movement::Left,
+                Movement::Right,
+            ],
+        }],
+        &[
+            Rot::R0,
+            Rot::R90,
+            Rot::R180,
+            Rot::R270,
+            Rot::R0,
+            Rot::R270,
+            Rot::R180,
+            Rot::R0,
+            Rot::R90,
+            Rot::R90,
+            Rot::R90,
+            Rot::R90,
+            Rot::R0,
+            Rot::R270,
+            Rot::R180,
+            Rot::R270,
+            Rot::R0,
+            Rot::R180,
+            Rot::R90,
+            Rot::R0,
+        ],
+    );
+
+    assert_eq!(expected, actual);
 }
