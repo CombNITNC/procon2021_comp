8c20afcf110e8d32b505f2d2945f0f6cfa4c8dab
you-mo77 <86389597+you-mo77@users.noreply.github.com>
2021-09-13 14:46:49 +0900

    修正
    ライフタイム？考慮

--- src/kaitou.rs
+++ src/kaitou.rs
@@ -1,37 +1,39 @@
-use crate::basis::{Operation, Rot, Movement};
+use crate::basis::{Movement, Operation, Rot};
 
-pub(crate) fn ans(ope:&[Operation], rot:&[Rot]) -> String{
-    let mut result= String::new();
+pub(crate) fn ans(ope: &[Operation], rot: &[Rot]) -> String {
+    let mut result = String::new();
 
     //回転情報
-    for i in rot{
-        result += match i {
-            Rot::R0 => &format!("{}",0),
-            Rot::R90 => &format!("{}",1),
-            Rot::R180 => &format!("{}",2),
-            Rot::R270 => &format!("{}",3),
+    for i in rot {
+        let r = match i {
+            Rot::R0 => 0,
+            Rot::R90 => 1,
+            Rot::R180 => 2,
+            Rot::R270 => 3,
         };
+        result += &format!("{}",r);
     }
     result += &format!("/r/n");
 
     //選択回数
-    result += &format!("{}/r/n",ope.len());
+    result += &format!("{}/r/n", ope.len());
 
-    for i in ope{
-        //選択画像位置 x(),y()の使い方違うかも
-        result += &format!("{}{}/r/n",i.select.x(),i.select.y());
+    for i in ope {
+        //選択画像位置
+        result += &format!("{}{}/r/n", i.select.x(), i.select.y());
 
         //交換回数
-        result += &format!("{}/r/n",i.movements.len());
-        
+        result += &format!("{}/r/n", i.movements.len());
+
         //交換操作
-        for j in 0..(i.movements.len() - 1){
-            result += match i.movements[j]{
-                Movement::Up => &format!("{}",'U'),
-                Movement::Right => &format!("{}",'R'),
-                Movement::Down => &format!("{}",'D'),
-                Movement::Left => &format!("{}",'L'),
+        for j in 0..(i.movements.len() - 1) {
+            let m = match i.movements[j] {
+                Movement::Up => 'U',
+                Movement::Right => 'R',
+                Movement::Down => 'D',
+                Movement::Left => 'L',
             };
+            result += &format!("{}",m);
         }
         result += &format!("/r/n");
     }
