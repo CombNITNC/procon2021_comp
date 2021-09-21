867693691da2a338360ab450369d09fa12d92574
MikuroXina <ryosukadnak@gmail.com>
2021-09-21 12:19:51 +0900

    test: Add test_map

--- src/fragment/map_fragment.rs
+++ src/fragment/map_fragment.rs
@@ -16,3 +16,36 @@ pub(crate) fn map_fragment(matched: &VecOnGrid<Fragment>) -> Vec<(Pos, Pos)> {
     }
     map
 }
+
+#[test]
+fn test_map() {
+    use crate::{basis::Rot, fragment::Edges, grid::Grid};
+
+    let grid = Grid::new(2, 2);
+    let case = {
+        let mut vec = VecOnGrid::with_init(
+            grid,
+            Fragment {
+                pos: grid.pos(0, 0),
+                rot: Rot::R0,
+                edges: Edges::new(vec![], vec![], vec![], vec![]),
+                pixels: vec![],
+            },
+        );
+        vec[grid.pos(0, 0)].pos = grid.pos(1, 1);
+        vec[grid.pos(1, 0)].pos = grid.pos(0, 0);
+        vec[grid.pos(0, 1)].pos = grid.pos(1, 0);
+        vec[grid.pos(1, 1)].pos = grid.pos(0, 1);
+        vec
+    };
+
+    let expected: Vec<(Pos, Pos)> = vec![
+        (grid.pos(0, 0), grid.pos(1, 1)),
+        (grid.pos(1, 0), grid.pos(0, 0)),
+        (grid.pos(0, 1), grid.pos(1, 0)),
+        (grid.pos(1, 1), grid.pos(0, 1)),
+    ];
+    let actual = map_fragment(&case);
+
+    assert_eq!(expected, actual);
+}
