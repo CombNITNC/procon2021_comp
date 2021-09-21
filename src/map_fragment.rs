fec45e34e2e708f6a3db9ca6b7a990b2247f387e
MikuroXina <ryosukadnak@gmail.com>
2021-09-21 12:07:21 +0900

    fix: Fix map_fragment arg

--- src/map_fragment.rs
+++ src/map_fragment.rs
@@ -6,14 +6,12 @@ use crate::{
 /// 原画像とマッチング後の画像を使って, 完成形からどのように断片画像の位置が移動したかを求める.
 ///
 /// [`crate::move_resolve::resolve`] の `movements` の形式に等しい.
-pub(crate) fn map_fragment(
-    original: &VecOnGrid<Fragment>,
-    matched: &VecOnGrid<Fragment>,
-) -> Vec<(Pos, Pos)> {
+pub(crate) fn map_fragment(matched: &VecOnGrid<Fragment>) -> Vec<(Pos, Pos)> {
     let mut map = vec![];
-    for (before, after) in original.iter().zip(matched.iter()) {
-        if before.pos != after.pos {
-            map.push((after.pos, before.pos));
+    for (pos, frag) in matched.iter_with_pos() {
+        // frag.pos の位置から pos の位置へ移動してきた
+        if frag.pos != pos {
+            map.push((pos, frag.pos));
         }
     }
     map
