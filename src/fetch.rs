ae6c54d4a3a61bf37cfef10151ca0a9bd62bfe62
MikuroXina <ryosukadnak@gmail.com>
2021-10-01 19:22:21 +0900

    fix: Fix to recieve endpoint as parameter

--- src/fetch.rs
+++ src/fetch.rs
@@ -1,8 +1,7 @@
-use std::{env, io};
+use std::io;
 
-pub(crate) fn fetch_ppm() -> reqwest::Result<impl io::BufRead> {
-    let mut endpoint =
-        env::var("ENDPOINT").expect("ENDPOINT var needed to fetch the problem ppm image");
+pub(crate) fn fetch_ppm(endpoint: &str) -> reqwest::Result<impl io::BufRead> {
+    let mut endpoint = endpoint.to_owned();
     endpoint.push_str("/problem.ppm");
     reqwest::blocking::get(endpoint).map(io::BufReader::new)
 }
