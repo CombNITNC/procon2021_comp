f8163b8c1f36ab1707a1ddd1b970c6071c7dabc2
kawaemon <me@kawaemon.dev>
2021-10-05 12:52:15 +0900

    refactor shaker

--- src/pixel_match/shaker.rs
+++ src/pixel_match/shaker.rs
@@ -3,6 +3,7 @@ use crate::{
     basis::Dir,
     fragment::{Edge, Fragment},
 };
+use std::cell::RefCell;
 
 fn find_by_single_side<'a, B>(
     fragments: &[Fragment],
@@ -30,6 +31,62 @@ where
     })
 }
 
+struct Context<'a> {
+    hints: RefCell<&'a mut ResolveHints>,
+    num_fragment: u8,
+    fragments: RefCell<&'a mut Vec<Fragment>>,
+    root_ref: &'a Fragment,
+}
+
+struct Finder<'a> {
+    dir: Dir,
+    list: &'a RefCell<Vec<Fragment>>,
+    oppisite_list: &'a RefCell<Vec<Fragment>>,
+    ctx: &'a Context<'a>,
+}
+
+impl<'a> Finder<'a> {
+    fn apply_confirmed_pairs(&mut self) {
+        let fragment_pos = self.list.borrow().last().unwrap_or(self.ctx.root_ref).pos;
+
+        if let Some(pairs) = self
+            .ctx
+            .hints
+            .borrow_mut()
+            .confirmed_pairs_of(EdgePos::new(fragment_pos, self.dir))
+        {
+            if self.list.borrow().len() + self.oppisite_list.borrow().len() + pairs.len() + 1
+                > self.ctx.num_fragment as usize
+            {
+                println!("shaker_fill: couldn't apply confirmed_pairs because of size overrun");
+            } else {
+                for (pos, rot) in pairs {
+                    let mut fragment =
+                        find_and_remove(*self.ctx.fragments.borrow_mut(), pos).unwrap();
+                    fragment.rotate(rot);
+                    self.list.borrow_mut().push(fragment);
+                }
+            }
+        }
+    }
+
+    fn find_match(&self) -> DiffEntry {
+        let list_ref = self.list.borrow();
+        let fragment_ref = list_ref.last().unwrap_or(self.ctx.root_ref);
+        find_by_single_side(
+            *self.ctx.fragments.borrow(),
+            fragment_ref.edges.edge(self.dir),
+            self.ctx.hints.borrow().blacklist_of(fragment_ref),
+        )
+    }
+
+    fn adopt(&mut self, d: DiffEntry) {
+        let mut fragment = find_and_remove(*self.ctx.fragments.borrow_mut(), d.pos).unwrap();
+        fragment.rotate(self.dir.calc_rot(d.dir));
+        self.list.borrow_mut().push(fragment);
+    }
+}
+
 /// root_ref から left_dir と left_dir.opposite() 方向に探索して、スコアが良い順に採用する。
 pub(super) fn shaker_fill(
     num_fragment: u8,
@@ -38,70 +95,46 @@ pub(super) fn shaker_fill(
     root_ref: &Fragment,
     hints: &mut ResolveHints,
 ) -> (Vec<Fragment>, Vec<Fragment>) {
-    let num_fragment = num_fragment as usize;
+    let (left, right) = (RefCell::new(vec![]), RefCell::new(vec![]));
 
-    let right_dir = left_dir.opposite();
-    let (mut left, mut right) = (vec![], vec![]);
-    let (mut left_fragment_ref, mut right_fragment_ref) = (root_ref, root_ref);
+    let ctx = Context {
+        hints: RefCell::new(hints),
+        num_fragment,
+        fragments: RefCell::new(fragments),
+        root_ref,
+    };
 
-    while right.len() + left.len() + (1/* for root */) != num_fragment {
-        if let Some(pairs) = hints.confirmed_pairs_of(EdgePos::new(left_fragment_ref.pos, left_dir))
-        {
-            if right.len() + left.len() + pairs.len() + 1 > num_fragment {
-                println!("shaker_fill: couldn't apply confirmed_pairs because of size overrun");
-            } else {
-                for (pos, rot) in pairs {
-                    let mut fragment = find_and_remove(fragments, pos).unwrap();
-                    fragment.rotate(rot);
-                    left.push(fragment);
-                }
-                left_fragment_ref = left.last().unwrap();
-            }
-        }
+    let mut left_finder = Finder {
+        dir: left_dir,
+        list: &left,
+        oppisite_list: &right,
+        ctx: &ctx,
+    };
 
-        if let Some(pairs) =
-            hints.confirmed_pairs_of(EdgePos::new(right_fragment_ref.pos, right_dir))
-        {
-            if right.len() + left.len() + pairs.len() + 1 > num_fragment {
-                println!("shaker_fill: couldn't apply confirmed_pairs because of size overrun");
-            } else {
-                for (pos, rot) in pairs {
-                    let mut fragment = find_and_remove(fragments, pos).unwrap();
-                    fragment.rotate(rot);
-                    right.push(fragment);
-                }
-                right_fragment_ref = right.last().unwrap();
-            }
-        }
+    let mut right_finder = Finder {
+        dir: left_dir.opposite(),
+        list: &right,
+        oppisite_list: &left,
+        ctx: &ctx,
+    };
 
-        if right.len() + left.len() + 1 == num_fragment {
+    while right.borrow().len() + left.borrow().len() + (1/* for root */) != num_fragment as usize {
+        right_finder.apply_confirmed_pairs();
+        left_finder.apply_confirmed_pairs();
+
+        if right.borrow().len() + left.borrow().len() + 1 == num_fragment as usize {
             break;
         }
 
-        let right_score = find_by_single_side(
-            fragments,
-            right_fragment_ref.edges.edge(right_dir),
-            hints.blacklist_of(right_fragment_ref),
-        );
-
-        let left_score = find_by_single_side(
-            fragments,
-            left_fragment_ref.edges.edge(left_dir),
-            hints.blacklist_of(left_fragment_ref),
-        );
+        let right_score = right_finder.find_match();
+        let left_score = left_finder.find_match();
 
         if right_score.score < left_score.score {
-            let mut fragment = find_and_remove(fragments, right_score.pos).unwrap();
-            fragment.rotate(right_dir.calc_rot(right_score.dir));
-            right.push(fragment);
-            right_fragment_ref = right.last().unwrap();
+            right_finder.adopt(right_score);
         } else {
-            let mut fragment = find_and_remove(fragments, left_score.pos).unwrap();
-            fragment.rotate(left_dir.calc_rot(left_score.dir));
-            left.push(fragment);
-            left_fragment_ref = left.last().unwrap();
+            left_finder.adopt(left_score);
         }
     }
 
-    (left, right)
+    (left.into_inner(), right.into_inner())
 }
