a8ef5417253f33b79b1fab10e34cb4aef4326f21
MikuroXina <ryosukadnak@gmail.com>
2021-10-08 13:01:32 +0900

    feat: Add as_degrees

--- src/basis.rs
+++ src/basis.rs
@@ -137,6 +137,15 @@ impl Rot {
             _ => unreachable!(),
         }
     }
+
+    pub(crate) fn as_degrees(self) -> f64 {
+        match self {
+            Rot::R0 => 0.0,
+            Rot::R90 => 90.0,
+            Rot::R180 => 180.0,
+            Rot::R270 => 270.0,
+        }
+    }
 }
 
 impl Add for Rot {
